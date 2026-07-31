736ec66e5f03619e0ee5a3c9725bb100a95ca57b
kashaf955 <kashaftariq496@gmail.com>
2026-07-31 20:03:41 +0500

    Add error handling for Mongoose duplicate key and JWT errors

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -9,6 +9,21 @@ module.exports = (err, req, res, next) => {
         const message = `Resource not found. Invalid: ${err.path}: ${err.value}`;
         err = new ErrorHandler(message, 400);
     }
+    // Mongoose duplicate key error
+    if (err.code === 11000) {
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+        err = new ErrorHandler(message, 400);
+    }
+    // Wrong JWT error
+    if (err.name === 'JsonWebTokenError') {
+        const message = `Json Web Token is invalid, try again`;
+        err = new ErrorHandler(message, 400);
+    }
+    // JWT expired error
+    if (err.name === 'TokenExpiredError') {
+        const message = `Json Web Token is expired, try again`;
+        err = new ErrorHandler(message, 400);
+    }
 
     res.status(err.statusCode).json({
         success: false,
