cd1b2d5bc7eb57c8db5bc58b6d83038a276d231b
kashaf955 <kashaftariq496@gmail.com>
2026-08-05 20:02:33 +0500

    Add frontend setup with React and Vite, including initial components, ESLint configuration, and basic styling

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from '../views/Login';
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </Router>
+    )
+}
+export default App;
