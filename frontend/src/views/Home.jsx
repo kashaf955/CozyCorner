41fc812293d655de4f30c8fda247751ca8eabd8a
kashaf955 <kashaftariq496@gmail.com>
2026-08-08 23:05:57 +0500

    Add Footer component to Home view for improved layout

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header.jsx";
 import Hero from "../components/hero.jsx";
+import Footer from "../components/footer.jsx";
 
 const Home = () => {
   return (
     <div className="min-h-screen">
       <Header />
       <Hero />
+      <Footer />
     </div>
   );
 };
