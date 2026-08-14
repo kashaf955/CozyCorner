77add84c4dce34cdc7aa14531cf87252476c3c9c
kashaf955 <kashaftariq496@gmail.com>
2026-08-14 20:27:41 +0500

    Add functionality to retrieve a single order by ID

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -32,3 +32,18 @@ exports.newOrder = catchAsyncErrors(async (req, res, next) => {
     });
 });
 
+
+// Get Single Order
+ 
+exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
+    const order = await Order.findById(req.params.id).populate("user", "name email");
+
+    if(!order) {
+        return next(new ErrorHandler("Order not found", 404));
+    }
+
+    res.status(200).json({
+        success: true,
+        order,
+    });
+});
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { isAuthenticatedUser } = require("../middleware/auth.js");
-const { newOrder } = require("../controllers/orderController.js");
+const { newOrder, getSingleOrder } = require("../controllers/orderController.js");
 
 const router = express.Router();
 
 router.route("/order/new").post(isAuthenticatedUser, newOrder);
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
 
 module.exports = router;
