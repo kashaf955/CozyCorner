8ad693f99afd07d14298abfa907b680ead922acc
kashaf955 <kashaftariq496@gmail.com>
2026-08-14 18:20:08 +0500

    Enhance Order model

--- backend/models/ordermodel.js
+++ backend/models/ordermodel.js
@@ -2,34 +2,101 @@ import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
 
-    orderId: {
-        type: String,
-        required: true,
-        unique: true,
+    shippingInfo: {
+        address: {
+            type: String,
+            required: true,
+        },
+        city: {
+            type: String,
+            required: true,
+        },
+        state: {
+            type: String,
+            required: true,
+        },
+        country: {
+            type: String,
+            required: true,
+        },
+        pinCode: {
+            type: Number,
+            required: true,
+        },
+        phoneNo: {
+            type: Number,
+            required: true,
+        },
     },
-    userId: {
-        type: String,
-        required: true,
-    },
-    products: [
+    orderItems: [
         {
-            productId: {
+        name: {
                 type: String,
                 required: true,
             },
-            quantity: {
-                type: Number,
-                required: true,
-            },
-        }
-    ],  
-    totalAmount: {
-        type: Number,
+        price: {
+            type: Number,
+            required: true,
+        },
+        quantity: {
+            type: Number,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Product",
+            required: true,
+        },
+    }],  
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
         required: true,
     },
-    status: {
+    paymentInfo: {
+        id: {
+            type: String,
+            required: true,
+        },
+        status: {
+            type: String,
+        },
+    },
+    paidAt: {
+        type: Date,
+        required: true,
+    },
+    itemsPrice: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    taxPrice: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    shippingPrice: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    totalPrice: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    orderStatus: {
         type: String,
         required: true,
+        default: "Processing",
+    },
+    deliveredAt: {
+        type: Date,
     },
     createdAt: {
         type: Date,
@@ -37,4 +104,4 @@ const orderSchema = new mongoose.Schema({
     },
 });
 
-const Order = mongoose.model("Order", orderSchema);
+module.exports = mongoose.model("Order", orderSchema);
