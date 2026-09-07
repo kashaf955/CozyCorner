86c995b5d9bd9745d18c55d1d9a57f40cf09d8f5
kashaf955 <kashaftariq496@gmail.com>
2026-09-07 15:27:01 +0500

    Add Profile component for user profile management,

--- frontend/src/views/login.jsx
+++ frontend/src/views/login.jsx
@@ -2,6 +2,7 @@ import Header from "../components/layout/header.jsx";
 import Footer from "../components/layout/footer.jsx";
 import LoginComp from "../components/layout/LoginComp.jsx";
 import Metadata from "../components/layout/metadata.jsx";
+
 const Login = () => {
   return (
     <div className="min-h-screen bg-[#0f1714]">
